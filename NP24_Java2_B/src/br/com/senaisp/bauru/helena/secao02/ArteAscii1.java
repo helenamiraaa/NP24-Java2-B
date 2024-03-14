63aedcc4051298b6aad957d5fba79d76f8f4d68e
helenamiraaa <h96865020@gmail.com>
2024-03-13 22:12:55 -0300

    Conteúdo da aula 13-03-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/helena/secao02/ArteAscii1.java
@@ -0,0 +1,17 @@
+package br.com.senaisp.bauru.helena.secao02;
+
+public class ArteAscii1 {
+
+	public static void main(String[] args) {
+      System.out.println("  ▓▓▓▓  ");
+      System.out.println(" ▓    ▓ ");
+      System.out.println("▓ ▓  ▓ ▓");
+      System.out.println("▓      ▓");
+      System.out.println("▓ ▓  ▓ ▓");
+      System.out.println("▓  ▓▓  ▓ ");
+      System.out.println(" ▓    ▓");
+      System.out.println("  ▓▓▓▓");
+      System.out.println("");
+	}
+
+}
