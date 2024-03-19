d5b5a5dbfc2746513cec3d89bf928fd23ff9d312
helenamiraaa <h96865020@gmail.com>
2024-03-18 22:04:16 -0300

    Conteúdo da aula do dia 18-03-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/helena/secao03/TextosTest.java
@@ -0,0 +1,17 @@
+package br.com.senaisp.bauru.helena.secao03;
+
+public class TextosTest {
+
+	public static void main(String[] args) {
+		char letter1 = 'H';
+		char letter2 = 'e';
+		char letter3 = 'l';
+		char letter4 = 'l';
+		char letter5 = 'o';
+		//É difícil codificar sentenças longas
+		System.out.println("" + letter1 + letter2 +
+		letter3 + letter4 + letter5);
+		System.out.println((++letter1));
+	}
+
+}
